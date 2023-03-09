f987e6bc1ff47b93d2f9f0afc53311273adfc6f6
baturdincer <baturdincerr@gmail.com>
2023-03-09 22:23:45 +0300

    first commit

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,14 @@
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        Scanner scanner= new Scanner(System.in);
+        System.out.print("Yılı giriniz: ");
+        int yil= scanner.nextInt();
+        if(yil%4==0){
+            if(yil%100==0&&yil%400!=0) System.out.println(yil+" Bir artık yıl değildir");
+            else System.out.println(yil+" Bir artık yıldır");
+        }
+        else System.out.println(yil+" Bir artık yıl değildir");
     }
 }
